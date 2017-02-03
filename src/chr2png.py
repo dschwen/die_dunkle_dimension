3f58355bdc8a43e2536c53914c795f9dde266b75
Daniel Schwen <daniel@schwen.de>
2017-02-03 07:27:20 -0700

    add charset extractor

---
+++ src/chr2png.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import sys
+
+if len(sys.argv) != 2 :
+  print "Usage ./chr2png.py ddd.chra"
+  sys.exit(1)
+
+import png
+
+
+fh = open(sys.argv[1], 'rb')
+ba = bytearray(fh.read())
+fh.close()
+
+print len(ba), "bytes read"
+
+bits = [128, 64, 32, 16, 8, 4, 2, 1]
+image = []
+
+for y in range(128) :
+	row = []
+	for x in range(128) :
+		c = int(y/8) * 16 + int(x/8)
+		v = ba[2 + c*8 + y % 8] & bits[x % 8]
+
+		if v == 0 :
+			row.append((255,255,255))
+		else:
+			row.append((0,0,0))
+
+	image.append(row)
+
+png.from_array(image, 'RGB').save('%s.png' % sys.argv[1])
