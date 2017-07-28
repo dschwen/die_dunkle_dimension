16de191b60164a15a11ea8f9e347818f1cd12f7d
Daniel Schwen <daniel@schwen.de>
2017-07-28 10:39:15 -0600

    render world map fully

--- src/worldmap2png.py
+++ src/worldmap2png.py
@@ -2,39 +2,43 @@
 import sys
 import png
 from utils.palette import palette
+from utils.gfx import drawTile
 
+
+# load packed overworld map
 fh = open('c64/DDD1/ddd.mapa', 'rb')
 mapa = bytearray(fh.read())
 fh.close()
 
+# load overworld charset
+fh = open('c64/DDD1/ddd.chra', 'rb')
+chars = bytearray(fh.read())
+fh.close()
+
+# load sub-tile data
 fh = open('c64/DDD1/ddd.code', 'rb')
 code = bytearray(fh.read())
 fh.close()
 
 xsize = 52
-
 offset = 2
-if len(sys.argv) >=4 :
-  xsize = int(sys.argv[3])
 
 size = len(mapa)
-lines = int((size - offset) / xsize)
+ysize = int((size - offset) / xsize)
 
-image = []
+# allocate array for the final image
+image = [[(0,0,0) for i in range(xsize*3*16)] for j in range(ysize*16)]
 
-for y in range(lines) :
-  row = []
-  for x in range(xsize) :
-    # get super tile
-    supertile = mapa[offset + x + y * xsize]
-    for t in range(3) :
-      # the three subtiles are at 0x100 * t + 0xCD00
-      # to get the array index we subtract the memory offset of the file (0xC540)
-      # and add 2, because the data is preceeded by a two bye offset address on disk
-      tile = code[0x100 * t + 0xCD00 - 0xC540 + 2 + supertile]
-      color = palette[code[0xC800 - 0xC540 + 2 + tile]]
-      row.append(color)
-
-  image.append(row)
+for y in range(ysize) :
+    row = []
+    for x in range(xsize) :
+        # get super tile
+        supertile = mapa[offset + x + y * xsize]
+        for t in range(3) :
+            # the three subtiles are at 0x100 * t + 0xCD00
+            # to get the array index we subtract the memory offset of the file (0xC540)
+            # and add 2, because the data is preceeded by a two bye offset address on disk
+            tile = code[0x100 * t + 0xCD00 - 0xC540 + 2 + supertile]
+            drawTile(image, chars, tile, (x*3 + t) * 16, y*16)
 
 png.from_array(image, 'RGB').save('c64/DDD1/ddd.mapa.png')
